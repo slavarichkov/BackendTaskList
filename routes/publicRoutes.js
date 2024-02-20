6a832a3cf230fbc1e1b1b1392b70a4e1f3347064
Slava Richkov <v.v.richkov@yandex.ru>
2024-02-20 14:29:35 +0500

    feat: routes

---
+++ routes/publicRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router(); // создали роутер
+
+//Контроллеры 
+const {
+    getTasks,
+    createTask,
+    updateTask,
+    removeTask,
+} = require('../controllers/tasks/tasksControllers');
+
+// Валидация
+const {
+    getTasksValidation,
+    createTaskValidation,
+    updateTaskValidation,
+    removeTaskValidation
+} = require('../utils/validation/validation');
+
+//Роуты
+router.get('/get-tasks/:deviceId', getTasksValidation, getTasks); 
+router.post('/create-task/:deviceId', createTaskValidation, createTask); 
+router.put('/update-task/:deviceId', updateTaskValidation, updateTask); 
+router.delete('/remove-task/:deviceId', removeTaskValidation, removeTask); 
+
+module.exports = router;
